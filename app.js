e0a262e17205bcb5e6965f1d6b71a9234eabfb23
estibalizleza <157582069+estibalizleza@users.noreply.github.com>
2024-12-10 10:55:29 +0000

    ariketa

--- app.js
+++ app.js
@@ -20,9 +20,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/profile', profileRouter);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); //uploads karpeta ikusgai egoteko
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,48 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.redirect('form.html');
 });
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      const extension = path.extname(file.originalname); //fitxategiaren luzapena
+      cb(null, 'avatar-'+ uniqueSuffix + extension)
+    }
+  })
+
+  const fileFilter = function (req, file, cb) {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+      cb(null, true); // Fitxategia png denean onartu
+    } else {
+        cb(new Error('png edo jpg izan behar da'), false);//cb(null, false);
+    }
+  };
+
+  const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: 2 * 1024 * 1024 }, // 2MB tamaina limitea
+    fileFilter: fileFilter //beste filtroa aplikatu
+   })
+
+
 router.post('/', upload.single('avatar'), function (req, res, next) {
-    console.log(req.file)
-    // req.body will hold the text fields, if there were any
-    res.send("Jasota")
+    const userName = req.body.name;
+    const protocol = req.protocol; 
+    const host = req.get('host');
+    const fileUrl = `/uploads/${req.file.filename}`; 
+    res.send(`
+    <p>Zure izena: ${userName}</p>
+    <p>Fitxategia: <a href="${fileUrl}" target="_blank">${fileUrl}</a></p>
+  `);
 })
 
 
